de00fb12bc6d46103b47bf57057ce9bf89679eaa
Zucareli <anazucareliofc@gmail.com>
2024-11-20 19:57:32 -0300

    livrodereceitas

---
+++ scripts.js
@@ -0,0 +1,39 @@
+// $(document).ready(function(){
+//     $('.slider-comidas').slick({
+//         dots: false,             // Exibe indicadores de navegação (bolinhas)
+//         infinite: true,         // Loop infinito
+//         speed: 500,             // Velocidade da transição
+//         slidesToShow: 6,        // Número de slides visíveis
+//         slidesToScroll: 3,      // Número de slides para avançar ao clicar
+//         arrows: true,           // Ativa as setas de navegação
+//         adaptiveHeight: true    // Ajusta a altura automaticamente
+//     });
+// });
+
+
+
+
+
+
+
+
+
+// <div class="slider-comidas">
+// 	<link rel="stylesheet" type="text/css" src="livro.css"/>
+// 	<script type="text/javascript" src="scripts.js">
+		
+// 	$(document).ready(function(){
+// 		$('.slider-comidas').slick({
+// 			dots: false,             // Exibe indicadores de navegação (bolinhas)
+// 			infinite: true,         // Loop infinito
+// 			speed: 500,             // Velocidade da transição
+// 			slidesToShow: 6,        // Número de slides visíveis
+// 			slidesToScroll: 3,      // Número de slides para avançar ao clicar
+// 			arrows: true,           // Ativa as setas de navegação
+// 			adaptiveHeight: true    // Ajusta a altura automaticamente
+// 		});
+// 	});
+	
+// 	</script>
+
+
